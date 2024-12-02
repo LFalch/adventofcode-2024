0ff47c321324935de8e655535fa7d647b7abf009
LFalch <lucas@wasd.dk>
2024-12-02 23:12:57 +0100

    Try to do a better da y2

--- day02/src/part2.zig
+++ day02/src/part2.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
 const aoc = @import("aoc");
 
-var mem_buf: [2048]u8 = undefined;
-
 pub fn main() !void {
-    var gpa = std.heap.FixedBufferAllocator.init(&mem_buf);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
     var f = try aoc.read_input();
@@ -12,79 +11,70 @@ pub fn main() !void {
 
     var numSafe: u64 = 0;
 
-    while (!f.is_done()) {
-        var list = std.ArrayList(u8).init(alloc);
+    big: while (!f.is_done()) {
+        std.debug.print("\n", .{});
+        var list = std.ArrayList(i8).init(alloc);
         defer list.deinit();
 
         var newLine = false;
         while (!newLine) : (newLine = f.read_space()) {
-            try list.append(f.read_number(u8));
+            try list.append(@intCast(f.read_number(u8)));
         }
 
-        var unsafesRemoved: u8 = 0;
-        var lastNum: ?u8 = null;
-        var direction: ?bool = null;
-        for (list.items) |num| {
-            if (unsafesRemoved > 1) break;
-            var isSafe = true;
-            l: {
-                if (lastNum) |l_num| {
-                    const diff: i9 = @as(i9, num) - l_num;
-                    if (@abs(diff) == 0 or @abs(diff) > 3) {
-                        isSafe = false;
-                        break :l;
-                    }
-                    const sign = diff > 0;
-                    if (direction) |d| {
-                        if (sign != d) {
-                            isSafe = false;
-                            break :l;
-                        }
-                    } else direction = sign;
+        for (list.items[1..], 0..) |e, i| {
+            const d = e - list.items[i];
+            list.items[i] = d;
+            std.debug.print("{d} ", .{d});
+        }
+        list.items.len -= 1;
+
+        var posses: u16 = 0;
+        var negs: u16 = 0;
+        var tooBig: ?i8 = null;
+        var negN: ?i8 = null;
+        var posN: ?i8 = null;
+
+        for (list.items) |l| {
+            if (@abs(l) > 3) {
+                if (tooBig) |tb| {
+                    std.debug.print("  .{d}", .{tb});
+                    continue :big;
                 }
+                tooBig = l;
             }
-            if (!isSafe) {
-                unsafesRemoved += 1;
-            } else {
-                lastNum = num;
+            if (l > 0) {
+                posN = l;
+                posses += 1;
+            } else if (l < 0) {
+                negN = l;
+                negs += 1;
             }
         }
-        if (unsafesRemoved > 1) {
-            std.mem.reverse(u8, list.items);
-            unsafesRemoved = 0;
-            lastNum = null;
-            direction = null;
-            for (list.items) |num| {
-                if (unsafesRemoved > 1) break;
-                var isSafe = true;
-                l: {
-                    if (lastNum) |l_num| {
-                        const diff: i9 = @as(i9, num) - l_num;
-                        if (@abs(diff) == 0 or @abs(diff) > 3) {
-                            isSafe = false;
-                            break :l;
-                        }
-                        const sign = diff > 0;
-                        if (direction) |d| {
-                            if (sign != d) {
-                                isSafe = false;
-                                break :l;
-                            }
-                        } else direction = sign;
-                    }
+        if (tooBig) |n| {
+            if (n < 0) {
+                switch (posses) {
+                    0 => {},
+                    1 => if (negN != n) continue :big,
+                    else => continue :big,
                 }
-                if (!isSafe) {
-                    unsafesRemoved += 1;
-                } else {
-                    lastNum = num;
+            } else if (n > 0) {
+                switch (negs) {
+                    0 => {},
+                    1 => if (posN != n) continue :big,
+                    else => continue :big,
                 }
             }
         }
 
-        if (unsafesRemoved <= 1) {
+        if ((posses >= 0 and negs <= 1) or (negs >= 0 and posses <= 1)) {
             numSafe += 1;
+            std.debug.print(" x", .{});
         }
     }
     timer.stop();
     try std.io.getStdOut().writer().print("{d}\n", .{numSafe});
+    // 398
 }
+
+// handle the fact that an outlier diff needs to be replaced by summing it with the next
+// diff in order to get the correct diff
