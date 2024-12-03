5dec607290ca2bfaa528c4f4cac551e3008a1e87
LFalch <lucas@wasd.dk>
2024-12-03 22:18:24 +0100

    Add benchmark script

---
+++ bench.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+echo "Day 1"
+cd day01
+echo "Part 1"
+zig build run1 -Doptimize=ReleaseFast -- 2164381
+echo "Part 2"
+zig build run2 -Doptimize=ReleaseFast -- 20719933
+cd ..
+
+echo "Day 2"
+cd day02
+echo "Part 1"
+zig build run1 -Doptimize=ReleaseFast -- 332
+echo "Part 2"
+zig build run2 -Doptimize=ReleaseFast -- 398
+cd ..
+
+echo "Day 3"
+cd day03
+echo "Part 1"
+zig build run1 -Doptimize=ReleaseFast -- 189527826
+echo "Part 2"
+zig build run2 -Doptimize=ReleaseFast -- 63013756
+cd ..
