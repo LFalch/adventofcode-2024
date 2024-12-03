aaacf81c74aee92cd047c7eaa3a1359a109c9a86
LFalch <lucas@wasd.dk>
2024-12-03 16:32:42 +0100

    Make day 3 part 2 double as fast

--- day03/src/part2.zig
+++ day03/src/part2.zig
@@ -1,23 +1,6 @@
 const std = @import("std");
 const aoc = @import("aoc");
 
-fn next_ins(f: *aoc.FileData) ?enum { Do, Dont, Mul } {
-    while (true) {
-        const i = std.mem.indexOfAny(u8, f.file_data, "dm") orelse return null;
-        f.file_data = f.file_data[i..];
-
-        if (f.accept("mul(")) {
-            return .Mul;
-        } else if (f.accept("do(")) {
-            return .Do;
-        } else if (f.accept("don't(")) {
-            return .Dont;
-        } else {
-            f.file_data = f.file_data[1..];
-        }
-    }
-}
-
 pub fn main() !void {
     try aoc.main_with_bench(u32, {}, solve);
 }
@@ -27,25 +10,27 @@ fn solve(fd: aoc.FileData, _: void) u32 {
 
     var mul_enabled = true;
     var sum: u32 = 0;
-    loop: while (next_ins(&f)) |i| {
-        switch (i) {
-            .Do => {
-                if (!f.accept(")")) continue :loop;
-                mul_enabled = true;
-            },
-            .Dont => {
-                if (!f.accept(")")) continue :loop;
-                mul_enabled = false;
-            },
-            .Mul => {
+    while (true) {
+        if (mul_enabled) {
+            const i = std.mem.indexOfAny(u8, f.file_data, "dm") orelse break;
+            f.file_data = f.file_data[i..];
+
+            if (f.accept("mul(")) {
                 const l = f.read_number(u16);
-                if (!f.accept(",")) continue :loop;
+                if (!f.accept(",")) continue;
                 const r = f.read_number(u16);
-                if (!f.accept(")")) continue :loop;
-                if (mul_enabled) {
-                    sum += @as(u32, l) * @as(u32, r);
-                }
-            },
+                if (!f.accept(")")) continue;
+                sum += @as(u32, l) * @as(u32, r);
+            } else if (f.accept("don't()")) {
+                mul_enabled = false;
+            } else {
+                f.file_data = f.file_data[1..];
+            }
+        } else {
+            const needle = "do()";
+            const i = std.mem.indexOf(u8, f.file_data, needle) orelse break;
+            f.file_data = f.file_data[i + needle.len ..];
+            mul_enabled = true;
         }
     }
     return sum;
