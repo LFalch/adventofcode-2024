5e3bfb1f7e2b97d8086c4965ced2d9f0aba6abfa
LFalch <lucas@wasd.dk>
2024-12-07 07:38:20 +0100

    Make day 7 part 2 a lot faster and a little neater

--- day07/src/part2.zig
+++ day07/src/part2.zig
@@ -28,26 +28,19 @@ fn solve(fd: aoc.FileData, ctx: struct { std.mem.Allocator }) u64 {
                 break;
             }
         }
-        outer: for (0..(1 + @as(usize, 1) << @as(u6, @intCast((operands.items.len - 1) << 1)))) |n| {
+        const num_op_combinations = std.math.powi(usize, 3, operands.items.len - 1) catch unreachable;
+        for (0..num_op_combinations + 1) |n| {
             var sum: u64 = operands.items[0];
             var cur_operator = n;
             for (operands.items[1..]) |next_operand| {
-                switch (cur_operator & 3) {
+                switch (cur_operator % 3) {
                     0 => sum += next_operand,
                     1 => sum *= next_operand,
-                    2 => {
-                        var buf1: [64]u8 = undefined;
-                        var buf2: [32]u8 = undefined;
-                        const b1 = std.fmt.bufPrint(buf1[0 .. buf1.len / 2], "{d}", .{sum}) catch unreachable;
-                        const b2 = std.fmt.bufPrint(&buf2, "{d}", .{next_operand}) catch unreachable;
-                        @memcpy(buf1[b1.len..][0..b2.len], b2);
-                        sum = std.fmt.parseInt(u64, buf1[0 .. b1.len + b2.len], 10) catch unreachable;
-                    },
-                    3 => continue :outer,
+                    2 => sum = concat(sum, next_operand),
                     else => unreachable,
                 }
                 if (sum > exp_result) break;
-                cur_operator >>= 2;
+                cur_operator /= 3;
             }
             if (sum == exp_result) {
                 count += exp_result;
@@ -57,4 +50,13 @@ fn solve(fd: aoc.FileData, ctx: struct { std.mem.Allocator }) u64 {
     }
 
     return count;
+}
+
+fn concat(a: u64, b: u64) u64 {
+    var buf1: [64]u8 = undefined;
+    var buf2: [32]u8 = undefined;
+    const b1 = std.fmt.bufPrint(buf1[0 .. buf1.len / 2], "{d}", .{a}) catch unreachable;
+    const b2 = std.fmt.bufPrint(&buf2, "{d}", .{b}) catch unreachable;
+    @memcpy(buf1[b1.len..][0..b2.len], b2);
+    return std.fmt.parseInt(u64, buf1[0 .. b1.len + b2.len], 10) catch unreachable;
 }
