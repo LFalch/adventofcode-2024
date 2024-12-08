fb92ed297c2561eff85255a6f5311a0e8b3292e1
LFalch <lucas@wasd.dk>
2024-12-08 07:41:46 +0100

    Day 8

---
+++ day08/src/part1.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+const aoc = @import("aoc");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    try aoc.main_with_bench(u32, &arena, solve);
+}
+
+const Entry = struct { key: u8, value: std.BoundedArray(Pos, 4) };
+const Pos = struct { i8, i8 };
+
+fn solve(fd: aoc.FileData, arena: *std.heap.ArenaAllocator) u32 {
+    const alloc = arena.allocator();
+    defer _ = arena.reset(.retain_capacity);
+    const w = std.mem.indexOf(u8, fd.file_data, "\n").? + 1;
+
+    var antennae = std.MultiArrayList(Entry){};
+    for (fd.file_data, 0..) |c, i| {
+        if (std.ascii.isAlphanumeric(c)) {
+            const index = b: {
+                if (std.mem.indexOf(u8, antennae.items(.key), &.{c})) |index|
+                    break :b index
+                else {
+                    antennae.append(alloc, .{ .key = c, .value = std.BoundedArray(Pos, 4){} }) catch unreachable;
+                    break :b antennae.len - 1;
+                }
+            };
+            antennae.items(.value)[index].append(.{ @intCast(i % w), @intCast(@divTrunc(i, w)) }) catch unreachable;
+        }
+    }
+
+    var total: u32 = 0;
+
+    const grid = alloc.dupe(u8, fd.file_data) catch unreachable;
+
+    for (antennae.items(.value)) |value| {
+        // all anteanne with same feq
+        const antennas = value.slice();
+        for (antennas, 0..) |a, i| {
+            for (antennas[i + 1 ..]) |b| {
+                const dx = b[0] - a[0];
+                const dy = b[1] - a[1];
+
+                set_antinode(b[0] + dx, b[1] + dy, grid, w, &total);
+                set_antinode(a[0] - dx, a[1] - dy, grid, w, &total);
+            }
+        }
+    }
+    return total;
+}
+
+pub fn set_antinode(x: isize, y: isize, grid: []u8, w: usize, total: *u32) void {
+    if (x >= 0 and y >= 0 and x < w - 1 and y < w - 1) {
+        const c = &grid[@as(usize, @intCast(x)) + @as(usize, @intCast(y)) * w];
+        if (c.* != '#') {
+            c.* = '#';
+            total.* += 1;
+        }
+    }
+}
